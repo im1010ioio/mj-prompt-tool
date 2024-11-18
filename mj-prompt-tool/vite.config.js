5e9626a11d32b7cc71eb70a0dcb250964302a248
陳依琳 <im1010ioio@gmail.com>
2024-11-18 14:38:36 +0800

    add autoprefixer

--- mj-prompt-tool/vite.config.js
+++ mj-prompt-tool/vite.config.js
@@ -1,8 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,6 +21,11 @@ export default defineConfig({
       sass: {
         includePaths: ['src/scss']
       }
+    },
+    postcss: {
+      plugins: [
+        autoprefixer()
+      ]
     }
   }
 })
