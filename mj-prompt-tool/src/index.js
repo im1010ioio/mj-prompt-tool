58cf075316b3c9c1307d50ed9cb22a6ad3eee4a2
陳依琳 <im1010ioio@gmail.com>
2024-11-18 11:47:33 +0800

    新增 vite 專案

---
+++ mj-prompt-tool/src/index.js
@@ -0,0 +1,33 @@
+import './scss/index.scss'
+
+import { createApp, ref, nextTick, onMounted, computed } from 'vue';
+
+const vm = createApp({
+    setup() {
+        const styles = ref({}); // 用於儲存 JSON 資料
+        const test = ref("test");
+
+        // 在組件掛載後載入 JSON
+        onMounted(async () => {
+            try {
+                const response = await fetch('/data/style.json'); // 載入 style.json
+                if (!response.ok) {
+                    throw new Error('Failed to load JSON file');
+                }
+                styles.value = await response.json(); // 解析 JSON 並儲存到 styles
+            } catch (error) {
+                console.error(error);
+            }
+            console.log(styles.value);
+            console.log(test.value);
+        });
+
+        return {
+            styles,
+            test
+        };
+    }
+});
+
+vm.mount('#app');
+
